fbad11d3d43482d88a10071450bbcdce6f378fd1
jamaspy <aspin@me.com>
2023-01-20 14:11:50 +1100

    change

--- components/Button.jsx
+++ components/Button.jsx
@@ -5,7 +5,7 @@ const Button = ({ children, onClick, disabled }) => {
     <button
       disabled={disabled}
       onClick={onClick}
-      className="border border-white rounded px-4 py-2 hover:bg-black hover:text-white disabled:cursor-not-allowed disabled:hover:bg-slate-200"
+      className="border border-white rounded px-4 py-2 hover:bg-zinc-700 text-white disabled:cursor-not-allowed disabled:hover:bg-slate-200"
     >
       {children}
     </button>
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
-import { Space_Grotesk } from "@next/font/google";
+import { Cormorant_Garamond } from "@next/font/google";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
 import "../styles/globals.css";
-const space = Space_Grotesk({
+const space = Cormorant_Garamond({
   subsets: ["latin"],
   variable: "--font-space",
+  weight: ["300", "400", "700"],
 });
 function MyApp({ Component, pageProps }) {
   return (
