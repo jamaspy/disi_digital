1ba2da29357a1527ab708c3dbe258ff19423cbbc
jamaspy <aspin@me.com>
2023-06-15 20:39:24 +1000

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { Space_Grotesk } from "@next/font/google";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
 import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 const space = Space_Grotesk({
   subsets: ["latin"],
   variable: "--font-space",
@@ -11,6 +12,7 @@ function MyApp({ Component, pageProps }) {
     <main className={`${space.variable} font-sans`}>
       <ToastContainer />
       <Component {...pageProps} />
+      <Analytics />
     </main>
   );
 }
