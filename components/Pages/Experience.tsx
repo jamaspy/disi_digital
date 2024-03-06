1d9037ed54fbe6738fbb228e0c09f176d01121ef
James Aspinall <aspin@me.com>
2024-03-07 10:29:16 +1100

    update experience

--- components/Pages/Experience.tsx
+++ components/Pages/Experience.tsx
@@ -21,14 +21,12 @@ export const ExperiencePage = () => {
                     </p>
                   </div>
                   <div className="flex flex-col">
-                    <p className="text-sm font-light">Aug 23 - Present</p>
+                    <p className="text-sm font-light">Sep 23 - Present</p>
                     <p className="text-sm font-light text-right">Sydney</p>
                   </div>
                 </div>
                 <p className="mt-2 font-light">
-                  Irure laborum ad id nisi veniam enim mollit magna qui enim
-                  Lorem ad. Officia officia consequat officia excepteur sunt
-                  nostrud occaecat voluptate reprehenderit est pariatur laborum.
+                  A web based application offering virtual work experience programs using Typescript, NextJS, Tailwind & tRPC. 
                 </p>
               </div>
               <div className="w-full border-b border-highlight pb-4 mt-4">
@@ -40,14 +38,14 @@ export const ExperiencePage = () => {
                     </p>
                   </div>
                   <div className="flex flex-col">
-                    <p className="text-sm font-light">Aug 23 - Present</p>
+                    <p className="text-sm font-light">Jun 2022 - Sep 2023</p>
                     <p className="text-sm font-light text-right">Sydney</p>
                   </div>
                 </div>
                 <p className="mt-2 font-light">
-                  Irure laborum ad id nisi veniam enim mollit magna qui enim
-                  Lorem ad. Officia officia consequat officia excepteur sunt
-                  nostrud occaecat voluptate reprehenderit est pariatur laborum.
+                  The dedicated Frontend Developer working on 2 HealthTech platforms written in Typescript using NextJS. Styled using
+                  TailwindCSS and interfacing with the backend through GraphQL and Prisma. I was responsible for creating reusable, scalable
+                  components from Figma designs for new features.
                 </p>
               </div>
               <div className="w-full border-b border-highlight pb-4 mt-4">
@@ -59,14 +57,14 @@ export const ExperiencePage = () => {
                     </p>
                   </div>
                   <div className="flex flex-col">
-                    <p className="text-sm font-light">Aug 23 - Present</p>
+                    <p className="text-sm font-light">Jan 2022 - Jun 2022</p>
                     <p className="text-sm font-light text-right">Sydney</p>
                   </div>
                 </div>
                 <p className="mt-2 font-light">
-                  Irure laborum ad id nisi veniam enim mollit magna qui enim
-                  Lorem ad. Officia officia consequat officia excepteur sunt
-                  nostrud occaecat voluptate reprehenderit est pariatur laborum.
+                  Web developer interacting directly with Clients to create and maintain features with in the CMS. Often working on multiple
+                  concurrent projects using vanilla HTML, CSS and Javascript to create new pages and features for government and blue chip
+                  clients globally. React component for translated into the CMS and Storybook was used for component development.
                 </p>
               </div>
               <div className="w-full border-b border-highlight pb-4 mt-4">
@@ -74,18 +72,19 @@ export const ExperiencePage = () => {
                   <div className="flex flex-col">
                     <p className="font-semibold">AgriDigital</p>
                     <p className="text-sm font-light text-highlight">
-                      Snr Web Developer
+                      Frontend Engineer
                     </p>
                   </div>
                   <div className="flex flex-col">
-                    <p className="text-sm font-light">Aug 23 - Present</p>
+                    <p className="text-sm font-light">Aug 2019 - Jan 2022</p>
                     <p className="text-sm font-light text-right">Sydney</p>
                   </div>
                 </div>
                 <p className="mt-2 font-light">
-                  Irure laborum ad id nisi veniam enim mollit magna qui enim
-                  Lorem ad. Officia officia consequat officia excepteur sunt
-                  nostrud occaecat voluptate reprehenderit est pariatur laborum.
+                  Frontend Engineer working on a cloud-based commodity management platform for web & mobile. Creating reusable
+                  components, forms, and dashboards using React/React Native, Typescript, Tailwind, and Material UI as well as Redux & React
+                  Query for state management. I also lead the design and implementation of the Design System, utilising Figma, Storybook &
+                  npm as well as creating initial feature designs in Figma.
                 </p>
               </div>
             </div>
